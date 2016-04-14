732663dee759d8aa3c4f0e743b3f1e39ba1f6152
Rafael Nascim ento Bezerra <rafaelbezerra.dev@gmail.com>
2016-04-14 00:18:57 +0000

    setting hostname on server side

--- workspace/JavaRMI/src/RMIServer.java
+++ workspace/JavaRMI/src/RMIServer.java
@@ -11,10 +11,15 @@ public class RMIServer extends UnicastRemoteObject implements Hello
     public static void main(String args[]) 
     { 
     	// run "start rmiregistry" first
+	final String bindName = "HelloServer";
+	final String hostname = "155.246.137.73";
+	System.setProperty("java.rmi.server.hostname", hostname);
+	System.out.println("Hostname set to: " + hostname);
+	System.out.println("Bind name set to: " + bindName);
+
         try 
         { 
             RMIServer obj = new RMIServer(); 
-            String bindName = "HelloServer";
             // Bind this object instance to the name  
             Naming.rebind(bindName, obj);
             System.out.println("Object binded to name: " + bindName);
